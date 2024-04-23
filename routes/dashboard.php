67b7942bf4d623d1babbbfd4c2188f1a8de193d0
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-04-23 14:30:37 +0100

    Online voting system

---
+++ routes/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Voting System - Dashboard</title>
+</head>
+<body>
+    <h1>Online Voting System</h1>
+    <hr>
+    <h3></h3>
+
+</body>
+</html>
